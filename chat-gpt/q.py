fc4a91e9b65e4a45b5038b3502b15708620ce7ca
nolleh7707 <nolleh7707@gmail.com>
2023-02-12 23:20:02 +0900

    feat: set max_tokens

--- chat-gpt/q.py
+++ chat-gpt/q.py
@@ -6,32 +6,34 @@ import os
 import dotenv
 import openai
 from termcolor import colored
-from pandas import DataFrame
-from tabulate import tabulate
 
 dotenv.load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 print(colored('Question> ', 'cyan'))
-statement = input()
+
 try:
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=statement,
-        temperature=0.6,
-    )
-
-    df = DataFrame(response.choices)
-
-    print(colored(response.choices[0].text, 'green'))
-    print(colored(f'response len:{len(df)}', 'light_magenta'))
-
-    print('=====================')
-    
-    print(colored(str(tabulate(df, headers = 'keys', tablefmt='psql')), 'light_magenta'))
-
-    # print(colored(str(response), 'blue'))
-        
+    statement = input()
+    message = ''
+    while statement != 'exit':
+        if statement == 'c': 
+            statement = 'please continue'
+        elif len(message) > 0:
+            print(colored(message, 'light_magenta'))
+            print('=====================')
+            message = ''
+        print(colored(statement, 'cyan'))
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=statement,
+            temperature=0.6,
+            max_tokens=100
+        )
+        print(colored(response.choices[0].text, 'green'))
+        print('press (c): read more, (exit): quit')
+        statement = input()
+        message += response.choices[0].text
+    print(colored(message, 'light_magenta'))
 except Exception as e:
     print(colored(f'fail: {e}', 'red'))
 
